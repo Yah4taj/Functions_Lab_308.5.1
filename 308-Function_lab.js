87554e5ab880d19cc9fec9e0e2918c30bb95c987
Tijhala Miller <tijhalamiller@Tijhalas-MacBook-Pro.local>
2025-01-24 17:07:28 -0500

    update part 2 added part 3

--- 308-Function_lab.js
+++ 308-Function_lab.js
@@ -20,47 +20,63 @@ console.log(averageArray([15, 25, 35, 45, 55]));
 
 // Take an array of strings and return the longest string.
 
-const strArray = ["dancing, reading", "walking", "biking", "jogging"];
+const strArray = ["dancing", "reading", "walking", "biking", "jogging"];
 const strArray2= ["pastas","desserts", "snacks"];
 function longestString(strg){
     let longest="";
     for (i = 0; i< strg.length; i++){
-        if (strg[i].length > longest.length); {
-            longest.strg[i]
-            console.log(longest); 
+        if (strg[i].length > longest.length) {
+            longest=strg[i]
+    
         }
-    longestString(strArray);
-    longestString(strArray2);
+
+    }
+    console.log(longest); 
 };
+longestString(strArray);
+    longestString(strArray2);
 
 
 // Take an array of strings, and a number and return an array of the strings that are longer than the given number. 
 
+function stringLongerThan(strg, number) {
+    const results =[];
+    for (let i=0; i< strg.length; i++)  {
+        if (strg[i].length > number) {
+            results.push(strg[i])
+        }
+    }
+    return results;
+}
+console.log(stringLongerThan(strArray, 3));
+
+// Take a number, n, and print every number between 1 and n without using loops. Use recursion.
 
 function printUntiln(number) {
     let current = 1;
-    while ( current !== number){
-    // if (current === number ) {
-    //   console.log(current);
-    } if (current < number){
+
+   while (current !== number) {
+      
+     if (current < number){
         current++;
-        // console.log(current);
+       
 } else if (current > number) {
          current --;
 }else {
     console.log("Please insert numeric number");
-    
+    break
 }
         if (current !== number){
             console.log(current);
         } 
-
-         }
+    }
+         };
+        
   
-printUntiln();
+printUntiln(8);
 
 
-//Part 2    Thinking Merhodically
+//==============Part 2  Thinking Methodically=========
 
 const dataArray= 
 
@@ -80,13 +96,33 @@ console.log(sortedArray);
 
    //Map the array to change the “occupation” key to “job” and increment every age by 1.
 
+   const people= 
+   
+   [{ id: "42", name: "Bruce", occupation: "Knight", age: "41" },
+    { id: "48", name: "Barry", occupation: "Runner", age: "25" },
+    { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
+    { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
+    { id: "7", name: "Bilbo", occupation: "None", age: "111" }
+   ]
+
+    const updatedPeople =people.map(person =>({
+        ...person,
+        job: person.occupation,
+        occupation: undefined,
+        age: person + 1
+    }));
+
+    console.log(updatedPeople);
+
+       
 //    let mappedData = data.map(person => ({
 //     ...person,
 //     job: person.occupation,
 //     age: String(Number(person.age) + 1)
 //   }));
 //   console.log(mappedData);
-  
+
+
    
   //Use the reduce method to calculate the sum of the ages.
 
@@ -103,4 +139,67 @@ function  averageAge() {
 }
 // console.log(averageAge(["19", "25", "41", "58", "111"]));
 console.log(averageAge(["19", "25", "41", "58", "111"]));
+
+//Part 3: Thinking Critically
+
+
+// Take an object and increment its age field.
+
+ let person = {
+    name: "Blaine", 
+    job: "Qiz Master", 
+    age: "58"
 };
+
+    person.age ++;
+
+    console.log(person.age);
+
+
+
+// // Take an object, make a copy, and increment the age field of the copy. Return the copy.
+
+let person2 = {
+    name: "Blaine", 
+    job: "Qiz Master", 
+    age: "59"
+};
+let person2Copy ={...person};
+
+person2Copy.age += 1;
+
+ console.log(person2Copy);
+ 
+
+// For each of the functions above, if the object does not yet contain an age field, create one and set it to 0. 
+
+let employee = {
+    name: "Blaine", 
+    job: "Qiz Master", 
+    age: "59"
+};
+employee.age=0;
+
+console.log(employee);
+
+// Also, add (or modify, as appropriate) an updated_at field that stores a Date object with the current time.
+
+let employee2 = {
+    name: "Blaine", 
+    job: "Qiz Master", 
+    age: "59"
+};
+
+employee2.updated_at = new Date();
+
+
+console.log(employee2);
+
+
+
+// Thought experiment: since the Date object is an object, what would happen if we modified it in the copy of the object created in the second function using setTime() or another method? How could we circumvent potentially undesired behavior.
+
+
+//When the date object is changed in one copy, it  also changes in the original because date is passed by reference. A new date object must be created to avoid this issue. 
+
+ 
